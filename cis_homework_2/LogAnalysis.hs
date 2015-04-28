24ad514c77966309ac283e3c99dbf1466a769a96
Ian Whitney <whit0694@umn.edu>
2015-04-28 09:59:37 -0500

    Initial. First passing version of Exercise 1

---
+++ cis_homework_2/LogAnalysis.hs
@@ -0,0 +1,50 @@
+{-# OPTIONS_GHC -Wall #-}
+module LogAnalysis where
+
+import Log
+
+parseMessage :: String -> LogMessage
+parseMessage ('E':' ':ms) = createErrorFromLog ms
+parseMessage ('I':' ':ms) = createInfoFromLog ms
+parseMessage ('W':' ':ms) = createWarningFromLog ms
+parseMessage x = Unknown x
+
+createWarningFromLog :: String -> LogMessage
+createWarningFromLog x = LogMessage Warning timestamp content
+  where timestamp = (logTimeStamp (createLogParams x))
+        content = (logContent (createLogParams x))
+
+createInfoFromLog :: String -> LogMessage
+createInfoFromLog x = LogMessage Info timestamp content
+  where timestamp = (logTimeStamp (createLogParams x))
+        content = (logContent (createLogParams x))
+
+createErrorFromLog :: String -> LogMessage
+createErrorFromLog x = LogMessage (Error code) timestamp content
+  where code = (errorCode (createErrorParams x))
+        timestamp = (errorTimeStamp (createErrorParams x))
+        content = (errorContent (createErrorParams x))
+
+createLogParams :: String -> (TimeStamp, String)
+createLogParams x = (wordToInteger (head(words x)), unwords(drop 1(words x)))
+
+logTimeStamp :: (TimeStamp, String) -> TimeStamp
+logTimeStamp (timestamp, _) = timestamp
+logContent :: (TimeStamp, String) -> String
+logContent (_, content) = content
+
+createErrorParams :: String -> (Int, TimeStamp, String)
+createErrorParams x = (wordToInteger (head(words x)), wordToInteger (head(drop 1 (words x))), unwords(drop 2(words x)))
+
+errorParams :: (String, String, String) -> (Int, TimeStamp, String)
+errorParams (a,b,c) = (wordToInteger a,  wordToInteger b, c)
+
+errorCode :: (Int, TimeStamp, String) -> Int
+errorCode (id, _, _) = id
+errorTimeStamp :: (Int, TimeStamp, String) -> TimeStamp
+errorTimeStamp (_, timestamp, _) = timestamp
+errorContent :: (Int, TimeStamp, String) -> String
+errorContent (_, _, content) = content
+
+wordToInteger :: String -> Int
+wordToInteger n = read n :: Int
